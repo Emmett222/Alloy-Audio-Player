4c379604d569f5781d79e632dec069bd4d998d54
Emmett Grebe <emmettgrebe@gmail.com>
2026-05-09 15:50:34 -0400

    Getting started

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
 
 dependencies {
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
     testImplementation(libs.junit)
