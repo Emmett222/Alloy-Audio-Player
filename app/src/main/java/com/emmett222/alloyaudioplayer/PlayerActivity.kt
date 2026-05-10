d09d49e9a3791892cc3130cd7c2a8230dbdc5605
Emmett Grebe <emmettgrebe@gmail.com>
2026-05-10 11:18:07 -0400

    Basic play/pause/unpause functionality implemented.

--- app/src/main/java/com/emmett222/alloyaudioplayer/PlayerActivity.kt
+++ app/src/main/java/com/emmett222/alloyaudioplayer/PlayerActivity.kt
@@ -1,8 +1,12 @@
 package com.emmett222.alloyaudioplayer
 
+import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Environment
 import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,7 +17,44 @@ import java.io.File
 class PlayerActivity : AppCompatActivity() {
 
     var audioFile: File? = null
+    var isStart: Boolean = true;
 
+    companion object {
+        var mediaPlayer: MediaPlayer = MediaPlayer()
+
+        /**
+         * Plays the music.
+         */
+        fun play(file: File?) {
+            mediaPlayer.let {
+                if (!it.isPlaying) {
+                    mediaPlayer.apply {
+                        setDataSource(file?.path)
+                        prepare()
+                        start()
+                    }
+                }
+            }
+        }
+
+        /**
+         * Pauses the music.
+         */
+        fun pause() {
+            mediaPlayer.pause()
+        }
+
+        /**
+         * Unpauses the music.
+         */
+        fun unPause() {
+            mediaPlayer.start()
+        }
+    }
+
+    /**
+     * Runs on opening the view.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -28,24 +69,23 @@ class PlayerActivity : AppCompatActivity() {
 
         var titleString: TextView = findViewById(R.id.titleString)
         titleString.text = audioFile?.name
-    }
 
-    companion object {
-        var mediaPlayer: MediaPlayer? = null
+        var playBtn: ImageButton = findViewById(R.id.playBtn)
 
-        // Call this function when a file is tapped
-        fun playAudio(file: File) {
-            mediaPlayer?.let {
-                if (it.isPlaying) {
-                    it.stop()
-                }
-                it.release()
-            }
-            mediaPlayer = MediaPlayer().apply {
-                setDataSource(file.path)
-                prepare()
-                start()
+        playBtn.setOnClickListener {
+            if (PlayerActivity.mediaPlayer?.isPlaying == true) {
+                PlayerActivity.pause()
+                playBtn.setImageResource(R.drawable.play_arrow_24px)
+            } else {
+                PlayerActivity.unPause()
+                playBtn.setImageResource(R.drawable.pause_24px)
             }
         }
+
+        PlayerActivity.play(audioFile)
+        isStart = false;
     }
+
+
+
 }
