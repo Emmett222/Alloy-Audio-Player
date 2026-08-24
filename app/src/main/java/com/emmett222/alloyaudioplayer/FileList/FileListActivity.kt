8c0268bdc3e2697f4206604d18627d2d87c43e2a
Emmett Grebe <emmettgrebe@gmail.com>
2026-08-24 18:49:55 -0400

    Removed gestures.

--- app/src/main/java/com/emmett222/alloyaudioplayer/FileList/FileListActivity.kt
+++ app/src/main/java/com/emmett222/alloyaudioplayer/FileList/FileListActivity.kt
@@ -7,6 +7,7 @@ import android.view.GestureDetector
 import android.view.HapticFeedbackConstants
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewConfiguration
 import android.view.animation.AnimationUtils
 import android.widget.ImageButton
 import android.widget.LinearLayout
@@ -31,7 +32,7 @@ import kotlin.math.abs
  * Lists the files. Only shows audio files.
  *
  * @author Emmett Grebe
- * @version 8-21-2026
+ * @version 8-24-2026
  */
 class FileListActivity : AppCompatActivity() {
     companion object {
@@ -47,6 +48,7 @@ class FileListActivity : AppCompatActivity() {
     private lateinit var songTitleText: TextView
     private lateinit var currentFolder: File
     private lateinit var initialRootFolder: File
+    private lateinit var viewConfig: ViewConfiguration
 
     private var shortenTitles: Boolean = false
     private var doPgnte: Boolean = false
@@ -87,6 +89,7 @@ class FileListActivity : AppCompatActivity() {
         shortenTitles = SettingsChange.getShortMode(this)
 
         recyclerView.layoutManager = LinearLayoutManager(this)
+        viewConfig = ViewConfiguration.get(this)
 
         val path: String = SettingsChange.getDefaultFolder(this)
         currentFolder = File(path)
@@ -97,7 +100,6 @@ class FileListActivity : AppCompatActivity() {
             doPgnte = true
         }
 
-        setupGestures()
         loadDirectory(currentFolder)
         setupBtns()
         changeColors()
@@ -155,55 +157,6 @@ class FileListActivity : AppCompatActivity() {
         }
         recyclerView.adapter = adapter
         pgnteText.text = adapter.getCurrPage()
-    }
-
-    /**
-     * Sets up custom gestures for navigating the pages.
-     */
-    private fun setupGestures() {
-        // The 'object : ' syntax is used to create an anonymous class. It is a one time object that
-        // implements an interface or extends a class, without needing to create a new .kt file.
-        // Think of it as "Create an (object) that acts like (:) this class/interface (____) and
-        // let me customize it.
-        val gestureDetector =
-            GestureDetector(this, object : GestureDetector.SimpleOnGestureListener() {
-                override fun onFling(
-                    e1: MotionEvent?, // Start
-                    e2: MotionEvent,  // End
-                    velocityX: Float,
-                    velocityY: Float
-                ): Boolean {
-                    if (e1 == null) return false
-                    val diffY = e2.y - e1.y
-                    val diffX = e2.x - e1.x
-
-                    // Motion determining.
-                    // Swipe from left to right quickly.
-                    if ((e1.x < 300) && abs(diffX) > abs(diffY)) { // Left of screen right
-                        if ((diffX > 150) && (abs(velocityX) > 150)) { // Far enough and fast enough.
-                            pgntePrev()
-                        }
-                    }
-
-                    // Motion determining.
-                    // Swipe from right to left quickly.
-                    if ((e1.x > 300) && abs(diffX) < abs(diffY)) { // Right of screen left
-                        if ((diffX > 150) && (abs(velocityX) > 150)) { // Far enough and fast enough.
-                            pgnteNext()
-                        }
-                    }
-
-                    return false
-                }
-            })
-
-        // Intercept touches on the root view
-        // '_' represents the View parameter. Underscore is used to safely ignore it, since it is
-        // not needed.
-        findViewById<View>(R.id.main).setOnTouchListener { _, event ->
-            gestureDetector.onTouchEvent(event)
-            true
-        }
     }
 
     /**
